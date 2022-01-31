96844bd16e5cd65235e95972aae1746edd89e156
Jan <Jan.Peltzer@iCloud.com>
2022-01-31 17:16:27 +0100

    Add ArtNet Thread

---
+++ backend/src/main/java/de/jmpsoftware/backend/model/PipeEntry.java
@@ -0,0 +1,106 @@
+package de.jmpsoftware.backend.model;
+
+import de.jmpsoftware.backend.service.ArtNetService;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PipeEntry {
+    int valueDMX;
+    int channel;
+    int universe;
+    int command;
+
+    public void setValuesFromBytePack(byte[] bytes) {
+        command = (bytes[0] & 0xff);
+        valueDMX = (bytes[1] & 0xff);
+        channel = (bytes[2] & 0xff);
+        channel += (bytes[3] & 0x0F ) << 8;
+        universe = (bytes[3] & 0xF0 ) >> 4;
+    }
+    public void setBytePackFromValues(byte[] bytes) {
+        bytes[0] = (byte)command;
+        bytes[1] = (byte)valueDMX;
+        bytes[2] = (byte)(channel & 0x00FF);
+        bytes[3] = (byte)((channel & 0xFF00) >> 8);
+        bytes[3] += (byte)((universe ) << 4);
+    }
+
+    public int getPackedIntegerByValues() {
+        int val = valueDMX;
+        val += channel << 8;
+        val += universe << 20;
+        val += command << 24;
+        return val;
+    }
+
+    public void setValuesByPackedInteger(int packedInteger) {
+        valueDMX = (packedInteger & 0xFF);
+        channel = ((packedInteger & 0x00000FFF00) >> 8);
+        universe = ((packedInteger & 0x0000F00000) >> 20);
+        command = (packedInteger >> 24);
+    }
+    
+
+    public boolean isCommand(int checkCommand) {
+        return getCommand() == checkCommand;
+    }
+    public boolean getValuesValid() {
+        if(( channel >= ArtNetService.SIZE_UNIVERSE) || ( channel < 0))
+            return false;
+
+        return (universe < ArtNetService.COUNT_UNIVERSE) && (universe >= 0);
+    }
+
+    public int getValueDMX() {
+        return valueDMX;
+    }
+
+    public void setValueDMX(int valueDMX) {
+        this.valueDMX = valueDMX;
+    }
+
+    public int getChannel() {
+        return channel;
+    }
+
+    public void setChannel(int channel) {
+        this.channel = channel;
+    }
+
+    public int getUniverse() {
+        return universe;
+    }
+
+    public void setUniverse(int universe) {
+        this.universe = universe;
+    }
+
+    public int getCommand() {
+        return command;
+    }
+
+    public void setCommand(int command) {
+        this.command = command;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PipeEntry pipeEntry = (PipeEntry) o;
+        return valueDMX == pipeEntry.valueDMX && channel == pipeEntry.channel && universe == pipeEntry.universe && command == pipeEntry.command;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(valueDMX, channel, universe, command);
+    }
+}
---
+++ backend/src/test/java/de/jmpsoftware/backend/service/ArtNetThreadTest.java
@@ -0,0 +1,68 @@
+package de.jmpsoftware.backend.service;
+
+import de.jmpsoftware.backend.model.PipeEntry;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+class ArtNetThreadTest {
+
+    @Test
+    void checkPipeReadDoneCorrectTest() throws IOException {
+
+        //Given
+        ArtNetThread myThread = new ArtNetThread(new PipedInputStream());
+
+        //WHEN
+        boolean retBool = myThread.checkPipeReadDoneCorrect( 4);
+        //THEN
+        assertTrue(retBool);
+
+        //WHEN
+        retBool = myThread.checkPipeReadDoneCorrect( -1);
+        //THEN
+        assertFalse(retBool);
+
+        //WHEN
+        retBool = myThread.checkPipeReadDoneCorrect( 1);
+        //THEN
+        assertFalse(retBool);
+    }
+
+    @Test
+    void isPipeEntryCommandDMXTest(){
+        //Given
+        ArtNetThread myThread = new ArtNetThread(new PipedInputStream());
+
+        //WHEN
+        PipeEntry pipeEntry = PipeEntry.builder().command(ArtNetService.COMMAND_DMX).valueDMX(0).channel(0).universe(0).build();
+        boolean retBool = myThread.isPipeEntryCommandDMX( pipeEntry);
+        //THEN
+        assertTrue(retBool);
+
+        //WHEN
+        pipeEntry = PipeEntry.builder().command(ArtNetService.COMMANDO_DMX_NO_SEND).valueDMX(0).channel(0).universe(0).build();
+        retBool = myThread.isPipeEntryCommandDMX( pipeEntry);
+        //THEN
+        assertTrue(retBool);
+
+        //WHEN
+        pipeEntry = PipeEntry.builder().command(ArtNetService.COMMAND_NO_START).valueDMX(0).channel(0).universe(0).build();
+        retBool = myThread.isPipeEntryCommandDMX( pipeEntry);
+        //THEN
+        assertFalse(retBool);
+        //WHEN
+        pipeEntry = PipeEntry.builder().command(ArtNetService.COMMAND_NO_STOP).valueDMX(0).channel(0).universe(0).build();
+        retBool = myThread.isPipeEntryCommandDMX( pipeEntry);
+        //THEN
+        assertFalse(retBool);
+    }
+
+}
