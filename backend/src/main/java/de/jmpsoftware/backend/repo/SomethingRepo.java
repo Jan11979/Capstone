fdedecb2c40b89d6d0e0c1d36fb2359cdc01904f
Jan <Jan.Peltzer@iCloud.com>
2022-02-11 19:20:27 +0100

    remove unused files

--- backend/src/main/java/de/jmpsoftware/backend/repo/SomethingRepo.java
+++
@@ -1,9 +0,0 @@
-package de.jmpsoftware.backend.repo;
-
-import de.jmpsoftware.backend.model.Something;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SomethingRepo extends MongoRepository<Something,String> {
-}
