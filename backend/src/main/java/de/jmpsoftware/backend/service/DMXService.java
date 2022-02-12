ad02d69b97fbf8e92ae2b6324e3bf9024db244e2
Jan <Jan.Peltzer@iCloud.com>
2022-02-12 11:23:36 +0100

    Fix:/home/runner/work/Capstone/Capstone/backend/src/main/java/de/jmpsoftware/backend/service/DMXService.java:[34,75] cannot find symbol

--- backend/src/main/java/de/jmpsoftware/backend/service/DMXService.java
+++ backend/src/main/java/de/jmpsoftware/backend/service/DMXService.java
@@ -1,15 +1,15 @@
 package de.jmpsoftware.backend.service;
 
 import de.jmpsoftware.backend.model.DMXTable;
-import de.jmpsoftware.backend.model.db.FixtureDB;
-import de.jmpsoftware.backend.model.db.FixtureTemplate;
-import de.jmpsoftware.backend.model.db.UniverseItemDB;
 import de.jmpsoftware.backend.model.fader.*;
 import de.jmpsoftware.backend.model.frontendconnection.ActiveFixtureList;
 import de.jmpsoftware.backend.model.frontendconnection.DbCommandItem;
 import de.jmpsoftware.backend.model.frontendconnection.FaderItem;
+import de.jmpsoftware.backend.model.db.FixtureDB;
 import de.jmpsoftware.backend.repo.FixtureRepo;
+import de.jmpsoftware.backend.model.db.FixtureTemplate;
 import de.jmpsoftware.backend.repo.FixtureTemplateRepo;
+import de.jmpsoftware.backend.model.db.UniverseItemDB;
 import de.jmpsoftware.backend.repo.UniverseRepo;
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
@@ -25,15 +25,14 @@ public class DMXService {
     private static final Log LOG = LogFactory.getLog(DMXService.class);
 
     private final DMXTableService dmxTableService;
-    private final UniverseRepo universeRepo;
-    private final FixtureRepo fixtureRepo;
     private final FixtureTemplateRepo fixtureTamplateRepo;
+    private final FixtureRepo fixtureRepo;
+    private final UniverseRepo universeRepo;
     private final ArtNetService artNetService;
     private final List<FixtureDB> fixtureList;
 
 
     public DMXService(UniverseRepo universeRepo, FixtureRepo fixtureRepo, FixtureTemplateRepo fixtureTamplateRepo) throws IOException {
-
         this.universeRepo = universeRepo;
         this.fixtureRepo = fixtureRepo;
         this.fixtureTamplateRepo = fixtureTamplateRepo;
