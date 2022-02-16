05c068a4fcbf13e9a851f4ba4d23d442bf97d12e
Jan <Jan.Peltzer@iCloud.com>
2022-02-16 16:29:46 +0100

    Security

--- frontend/src/page/OptionsSider/AddFixture.tsx
+++ frontend/src/page/OptionsSider/AddFixture.tsx
@@ -5,7 +5,7 @@ import {ActiveFixtureItem, CreateFixtureItem} from "../../model/BackendConnectio
 import {Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField} from "@mui/material";
 
 
-function checkFixtureNameAllowed(list: ActiveFixtureList[], name: string): string {
+function checkFixtureNameAllowed(list: ActiveFixtureItem[], name: string): string {
     if (name.length <= 3) {
         return "short";
     }
